12f1bc7eafce7b0ae28319c67bfc7b31e274bceb
的場郁美 <>
2020-05-18 14:33:09 +0900

    Flower

---
+++ dist/app.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    // bodyを遅延で表示
+    var bodyIn= {
+        delay: 000,
+        distance: '500px',
+        origin: 'top',
+        };
+        ScrollReveal().reveal('body', bodyIn);
+
+        // $("body").fadeIn(3000);
+})
---
+++ dist/app1.js
@@ -0,0 +1,45 @@
+$(function(){
+    
+    
+    // 矢印がクリックされたら、スムーススクロール
+    $('.arrow-up').click(() => {
+        $('body, html').animate({ scrollTop: 0,}, 600);
+        
+    })
+    
+    // 画面がスクロールされたら
+    $(window).scroll(function () {
+        
+        // 画面のスクロールされた量を取得
+        let scrollTop = $(document).scrollTop();
+        console.log(scrollTop);
+        
+        // スクロールの量が500以上なら表示、それ以外は非表示
+        if (scrollTop >= 300) {
+            $('.arrow-up').fadeIn();
+        } else {
+            $('.arrow-up').fadeOut();
+        }
+
+
+        // 画面表示時アニメーション
+        let sectionTop = $('#birth-flower').offset().top;
+
+        if (scrollTop >= sectionTop) {
+            $('#birth-flower').animate({ left: 0}, 1000);
+        }
+
+        let asideTop = $('#flower-code').offset().top;
+
+        if (scrollTop >= asideTop -500) {
+            $('#flower-code').animate({ left: 0}, 1000);
+        }
+
+        let mainTop = $('#your-birth').offset().top;
+
+        if (scrollTop >= mainTop -500) {
+            $('#your-birth').animate({ left: 0}, 1000);
+        }
+        
+    });
+})
